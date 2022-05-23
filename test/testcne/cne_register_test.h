36751e78fc3b85ee1d8dbdc6d02581198b3382c0
Sushma Sitaram <sushma.sitaram@intel.com>
2022-05-23 08:32:50 -0700

    testcne: add functional tests for cne lib to testcne
    Add functional tests relevant to lib/core/cne/cne.[ch]
    to testcne. Functions which are covered by other testcne
    tests are not covered.
    
    Signed-off-by: Sushma Sitaram <sushma.sitaram@intel.com>

---
+++ test/testcne/cne_register_test.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2022 Intel Corporation
+ */
+
+#ifndef _CNE_REGISTER_TEST_H_
+#define _CNE_REGISTER_TEST_H_
+
+/**
+ * @file
+ * CNE Setup Test
+ *
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int cne_register_main(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _CNE_REGISTER_TEST_H_ */
