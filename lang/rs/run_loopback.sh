b908706fc98d03906678793534e965cd3bd5f9af
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2022-06-24 12:15:16 +0000

    Reorganize Rust directory.
    - Move high level CNDP CNE Rust library under apis directory.
    - Remove old unused rust projects like helloworld, mmap, ring_test.
    - Keep a top level Cargo.toml file to build Rust applications and libraries.
    
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

--- lang/rs/run_loopback.sh
+++ lang/rs/run_loopback.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 # Build CNDP and install (sudo CNE_DEST_DIR=/ make install) before running this script.
-# Usage: ./run.sh <json config file (optional)> <port id (optional)> <core id (optional)>
-# eg 1: ./run.sh -> Run using default values
-# eg 2:./run.sh ../../fwd.json 0 25 -> Run using user specified values.
+# Usage: ./run_loopback.sh <json config file (optional)> <port id (optional)> <core id (optional)>
+# eg 1: ./run_loopback.sh -> Run using default values
+# eg 2:./run_loopback.sh ../../fwd.json 0 25 -> Run using user specified values.
+CRATE=loopback
 
 # JSON file. Use default jsonc file in library crate.
-CONFIG=${1:-"../../fwd.jsonc"}
+CONFIG=${1:-"./apis/cne//fwd.jsonc"}
 
 # Port id. Use default port id as 0.
 PORT=${2:-0}
@@ -17,6 +18,6 @@ CORE=${3:-"-1"}
 # Need to LD_PRELOAD libpmd_af_xdp.so since Rust binary doesn't include it and is required for applications.
 # Including libpmd_af_xdp.so as whole-archive during linking of rust binary doesn't seem to work.
 cargo build --release
-sudo -E LD_LIBRARY_PATH=$LD_LIBRARY_PATH LD_PRELOAD=$LD_LIBRARY_PATH/libpmd_af_xdp.so RUST_LOG=info `which cargo` run --release -- -c $CONFIG -p $PORT -a $CORE
+sudo -E LD_LIBRARY_PATH=$LD_LIBRARY_PATH LD_PRELOAD=$LD_LIBRARY_PATH/libpmd_af_xdp.so RUST_LOG=info `which cargo` run -p $CRATE --release -- -c $CONFIG -p $PORT -a $CORE
 
 stty sane
