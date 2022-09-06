8aaec3b514eb147bd194323d41882c4e94186c62
Nicholas Waples <nwaples@gmail.com>
2022-09-06 16:31:08 +1000

    add cpu affinity to go bindings
    Signed-off-by: Nicholas Waples <nwaples@gmail.com>

--- lang/go/bindings/cne/system.go
+++ lang/go/bindings/cne/system.go
@@ -1,5 +1,6 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright (c) 2022 Intel Corporation.
+ * Copyright (c) 2022 Canopus Networks.
  */
 
 package cne
@@ -18,20 +19,29 @@ package cne
 import "C"
 
 import (
+	"errors"
 	"fmt"
-	"io"
 	"os"
 	"runtime"
-	"strings"
+	"sync"
 	"unsafe"
 
 	"github.com/tidwall/jsonc"
+	"golang.org/x/sys/unix"
+)
+
+const (
+	DefaultLCoreGroup = "default" // the lcore group used if a thread if not assigned to a group
+	MainThreadName    = "main"    // the thread name used to register the main CNDP thread
 )
 
 // System structure to hold internal information for binding layer to CNDP
 type System struct {
-	tid  int
-	jcfg *Config
+	jcfg     *Config
+	mu       sync.Mutex
+	tidMap   map[string]*int         // map of thread name to cne uid
+	groupSet map[string]*unix.CPUSet // map of lcore group name to cpu set to use when registering
+	oldSet   map[string]*unix.CPUSet // map of thread name to cpu set before the thread was registered
 }
 
 // Create and setup the UMEM and LPort structures
@@ -42,105 +52,77 @@ func (sys *System) initializeSystem() error {
 	}
 
 	if err := sys.setupLPorts(); err != nil {
+		sys.cleanupUMEM()
 		return err
 	}
 
 	return nil
 }
 
-// open processes the JSON-C configuration and initializes the binding layer
-//
-// The cfg argument can be a JSON string, path to JSON file or pointer to cne.Config structure
-// Returns an error or the binding layer System structure pointer
-func open(cfg interface{}) (*System, error) {
-
-	sys := &System{}
-
-	sys.tid = RegisterThread("main")
-	if sys.tid < 0 {
-		return nil, fmt.Errorf("failed to register main thread")
-	}
-
-	switch v := cfg.(type) {
-	case *Config: //  Initialize the binding layer with a cne.Config structure
-		jcfg := v
-
-		if err := jcfg.validateConfig(); err != nil {
-			UnregisterThread(sys.tid)
-			return nil, fmt.Errorf("failed to validate configuration")
-		}
-		sys.jcfg = jcfg
-
-	case []byte: // Initialize the binding layer with a JSON byte array
-		if jcfg, err := processConfig(v); err != nil {
-			UnregisterThread(sys.tid)
-			return nil, fmt.Errorf("failed to process configuration")
-		} else {
-			sys.jcfg = jcfg
-		}
-
-	default:
-		UnregisterThread(sys.tid)
-		return nil, fmt.Errorf("invalid argument type not *Config or []byte, %T", v)
-	}
-
-	return sys, sys.initializeSystem()
-}
-
 // OpenWithConfig by passing in a initialized cne.Config structure
 func OpenWithConfig(cfg *Config) (*System, error) {
+	if err := cfg.validateConfig(); err != nil {
+		return nil, fmt.Errorf("failed to validate configuration: %w", err)
+	}
 
-	return open(cfg)
+	sys := &System{jcfg: cfg}
+	sys.tidMap = make(map[string]*int)
+	sys.groupSet = make(map[string]*unix.CPUSet)
+	sys.oldSet = make(map[string]*unix.CPUSet)
+
+	// create cpu sets for lcore groups
+	for group, cores := range cfg.LCoreGroupData {
+		var cs unix.CPUSet
+		for _, n := range cores {
+			cs.Set(n)
+		}
+		sys.groupSet[group] = &cs
+	}
+
+	err := sys.RegisterThread(MainThreadName)
+	if err != nil {
+		return nil, err
+	}
+
+	err = sys.initializeSystem()
+	if err != nil {
+		sys.UnregisterThread(MainThreadName)
+		return nil, err
+	}
+	return sys, nil
+}
+
+func openWithBytes(b []byte) (*System, error) {
+	jcfg, err := processConfig(jsonc.ToJSON(b))
+	if err != nil {
+		return nil, fmt.Errorf("failed to process configuration: %w", err)
+	}
+	return OpenWithConfig(jcfg)
 }
 
 // OpenWithString by passing in a string containing JSON-C or JSON text
-func OpenWithString(cfgStr string) (*System, error) {
-
-	str := strings.TrimSpace(cfgStr)
-
-	// test for a JSON-C or JSON string, which must start with a '{'
-	if !strings.HasPrefix(str, "{") {
-		return nil, fmt.Errorf("string does not appear to be a valid JSON text missing starting '{'")
-	}
-
-	return open(jsonc.ToJSON([]byte(str)))
+func OpenWithString(s string) (*System, error) {
+	return openWithBytes([]byte(s))
 }
 
 // OpenWithFile by passing in a filename or path to a JSON-C or JSON configuration
 func OpenWithFile(path string) (*System, error) {
-
-	var err error
-
-	str := strings.TrimSpace(path)
-
-	// test for a JSON-C or JSON string, which must start with a '{'
-	if strings.HasPrefix(str, "{") {
-		err = fmt.Errorf("path does not appear to be a valid filepath")
-	} else {
-		var configFile *os.File
-
-		if configFile, err = os.Open(str); err == nil {
-			var bytes []byte
-
-			defer configFile.Close()
-
-			// Read the JSON-C file into a single byte array for later parsing
-			if bytes, err = io.ReadAll(configFile); err == nil {
-				return open(jsonc.ToJSON(bytes))
-			}
-		}
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
 	}
-
-	return nil, err
+	return openWithBytes(b)
 }
 
 // Close the binding layer and cleanup UMEM and LPorts
 func (sys *System) Close() error {
+	sys.mu.Lock()
+	defer sys.mu.Unlock()
 
-	if sys.tid < 0 {
-		return fmt.Errorf("system not initialized")
+	if sys.tidMap[MainThreadName] == nil {
+		return errors.New("system not initialized")
 	}
-	defer UnregisterThread(sys.tid)
+	defer sys.unregister(MainThreadName)
 
 	if err := sys.cleanupUMEM(); err != nil {
 		return fmt.Errorf("error cleaning up mempool %w", err)
@@ -160,21 +142,80 @@ func (sys *System) JsonCfg() *Config {
 }
 
 // RegisterThread locks a thread and registers it to CNDP to get the CNE UID value
-func RegisterThread(name string) int {
+func (sys *System) RegisterThread(name string) error {
+	sys.mu.Lock()
+	defer sys.mu.Unlock()
+
+	if sys.tidMap[name] != nil {
+		return fmt.Errorf("thread %s already registered", name)
+	}
 
 	cName := C.CString(name)
 	defer C.free(unsafe.Pointer(cName))
 
 	runtime.LockOSThread()
+	tid := int(C.cne_register(cName))
+	if tid < 0 {
+		runtime.UnlockOSThread()
+		return fmt.Errorf("failed to register %s thread", name)
+	}
+	sys.tidMap[name] = &tid
 
-	return int(C.cne_register(cName))
+	// get cpu set from thread lcore group
+	var newSet *unix.CPUSet
+	if threadInfo := sys.jcfg.ThreadInfoMap[name]; threadInfo != nil {
+		newSet = sys.groupSet[threadInfo.Group]
+	}
+	if newSet == nil {
+		newSet = sys.groupSet[DefaultLCoreGroup]
+		if newSet == nil {
+			return nil
+		}
+	}
+
+	// save current cpu affinity settings
+	var oldSet unix.CPUSet
+	err := unix.SchedGetaffinity(0, &oldSet)
+	if err != nil {
+		sys.unregister(name)
+		return err
+	}
+	sys.oldSet[name] = &oldSet
+
+	// set new cpu affinity settings
+	err = unix.SchedSetaffinity(0, newSet)
+	if err != nil {
+		sys.unregister(name)
+		return err
+	}
+	return nil
+}
+
+func (sys *System) unregister(name string) error {
+	tid := sys.tidMap[name]
+	if tid == nil {
+		return fmt.Errorf("thread %s not registered", name)
+	}
+
+	if ret := int(C.cne_unregister(C.int(*tid))); ret < 0 {
+		return fmt.Errorf("cne_unregister(%v) failed", tid)
+	}
+	delete(sys.tidMap, name)
+
+	runtime.UnlockOSThread()
+
+	oldSet := sys.oldSet[name]
+	if oldSet != nil {
+		delete(sys.oldSet, name)
+		return unix.SchedSetaffinity(0, oldSet)
+	}
+	return nil
 }
 
 // UnregisterThread unregister the thread from CNDP
-func UnregisterThread(tid int) {
+func (sys *System) UnregisterThread(name string) error {
+	sys.mu.Lock()
+	defer sys.mu.Unlock()
 
-	if ret := int(C.cne_unregister(C.int(tid))); ret < 0 {
-		fmt.Printf("cne_unregister(%v) failed", tid)
-	}
-	runtime.UnlockOSThread()
+	return sys.unregister(name)
 }
