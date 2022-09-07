91caeae278c6df62c592f5647952ecc225eb8eef
Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>
2022-09-07 13:07:38 +0000

    Write generated bindings from build.rs in OUT directory.
    Rust cargo publish doesn't allow source directory to be modified by build.rs.
    If we do a dry run for cargo publish we will get following error.
    "Build scripts should not modify anything outside of OUT_DIR."
    So write generated bindings file in build.rs in OUT directory.
    
    Signed-off-by: Manoj Gopalakrishnan <manoj.gopalakrishnan@intel.com>

--- lang/rs/bindings/cne-sys/src/lib.rs
+++ lang/rs/bindings/cne-sys/src/lib.rs
@@ -14,11 +14,6 @@
 //!
 //! Also see the high-level CNE API crate for CNDP [GitHub](https://github.com/CloudNativeDataPlane/cndp/tree/main/lang/rs/apis/cne)
 
-#[allow(non_upper_case_globals)]
-#[allow(non_camel_case_types)]
-#[allow(non_snake_case)]
-#[allow(dead_code)]
-#[allow(improper_ctypes)]
 pub mod bindings;
 
 #[cfg(test)]
