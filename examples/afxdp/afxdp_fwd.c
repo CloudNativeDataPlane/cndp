345d306e52b58008b0e05eecad4527fa6b8c1c4b
Keith Wiles <keith.wiles@intel.com>
2022-10-06 13:34:36 -0500

    add kernel example afxdp_fwd build
    Build the kernel af_xdp sample code xsk_fwd.c along with the xdpsock_user.c
    example code. The examples/afxdp_user directory renamed to examples/afxdp
    and build xsk_fwd.c code as afxdp_fwd along with the afxdp_user example.
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

---
+++ examples/afxdp/afxdp_fwd.c
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2021-2022 Intel Corporation.
+ */
+
+#undef _GNU_SOURCE
+typedef unsigned long long u64;
+typedef unsigned int u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
+
+typedef __signed__ int s32;
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wincompatible-pointer-types-discards-qualifiers"
+#else
+#pragma GCC diagnostic ignored "-Wdiscarded-qualifiers"
+#endif
+#pragma GCC diagnostic ignored "-Wformat-nonliteral"
+#pragma GCC diagnostic ignored "-Wsign-compare"
+
+/*
+ * This file is located in the /usr/src/linux-source-X.Y.Z/samples/bpf directory.
+ *
+ * The location is defined in the cndp/meson_options.txt file.
+ */
+#include "xsk_fwd.c"
