de4660dcd3f133325862a7c88c767f283ffcfa9d
Keith Wiles <keith.wiles@intel.com>
2023-05-08 16:53:46 -0500

    Update l3-fwd.c
    Remove malloc() calls in the data path.
    
    Signed-off-by: Keith Wiles <keith.wiles@intel.com>

--- examples/cndpfwd/l3-fwd.c
+++ examples/cndpfwd/l3-fwd.c
@@ -78,10 +78,8 @@ l3fwd_fib_populate(struct fwd_info *fwd, struct cne_fib *fib)
 int
 l3fwd_fib_lookup(uint32_t *ip, struct ether_addr *eaddr, uint16_t *tx_port, int n)
 {
-    uint64_t *nexthop = malloc(n * sizeof(uint64_t));
-    if (nexthop == NULL) 
-        return -1; 
-
+    uint64_t nhop[n], *nexthop = &nhop[0];
+    
     memset(nexthop, 0, n); 
 
     cne_fib_lookup_bulk(fib, ip, nexthop, n);
