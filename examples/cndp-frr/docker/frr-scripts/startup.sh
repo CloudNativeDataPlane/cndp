a5861a0915a75dbf5033d5ec2efc1ae7446cb14b
Maryam Tahhan <mtahhan@redhat.com>
2023-11-10 09:16:39 -0500

    cndp-frr: fixup super linter
    Signed-off-by: Maryam Tahhan <mtahhan@redhat.com>

--- examples/cndp-frr/docker/frr-scripts/startup.sh
+++ examples/cndp-frr/docker/frr-scripts/startup.sh
@@ -3,9 +3,11 @@
 # Copyright (c) Red Hat Inc.
 
 sysctl -p /etc/sysctl.d/90-routing-sysctl.conf
+# shellcheck source=/dev/null
 source logging.sh
+# shellcheck source=/dev/null
 source /usr/lib/frr/frrcommon.sh
 daemon_list
 all_start
 all_status
-/usr/lib/frr/watchfrr -d $(daemon_list)
+/usr/lib/frr/watchfrr -d "$(daemon_list)"
